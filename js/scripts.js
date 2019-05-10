c9ee5a3ea1b1232e9ae7e1ea9e80287f70124b01
Panca Satrio Nugroho <nugroho.panca@gmail.com>
2019-05-10 11:21:38 +0700

    add archive page with list js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 jQuery(function($) {
 
-    $(document).ready(function() {
+    $(window).on('load',function() {
 
         // Mobile Menu
         $('.mobile_menu--button_container--col .circle').click(function() {
@@ -64,6 +64,135 @@ jQuery(function($) {
             }
         });
 
-    })
+        // Blog Archives
+
+        var itemSelector = '.blog--item'
+
+        var $container = $('#blog_archives--list').isotope({
+            itemSelector: itemSelector
+        });
+
+        var responsiveIsotope = [
+            [480, 4],
+            [720, 6]
+        ];
+    
+        var itemsPerPageDefault = 2;
+        var itemsPerPage = defineItemsPerPage();
+        var currentNumberPages = 1;
+        var currentPage = 1;
+        var currentFilter = '*';
+        var filterAtribute = 'data-filter';
+        var pageAtribute = 'data-page';
+        var pagerClass = 'isotope-pager';
+    
+        function changeFilter(selector) {
+            console.log(selector);
+            $container.isotope({
+                filter: selector
+            });
+        }
+    
+    
+        function goToPage(n) {
+            currentPage = n;
+    
+            var selector = itemSelector;
+                selector += ( currentFilter != '*' ) ? currentFilter : '';
+                selector += '['+pageAtribute+'="'+currentPage+'"]';
+    
+            changeFilter(selector);
+        }
+    
+        function defineItemsPerPage() {
+            var pages = itemsPerPageDefault;
+    
+            for( var i = 0; i < responsiveIsotope.length; i++ ) {
+                if( $(window).width() <= responsiveIsotope[i][0] ) {
+                    pages = responsiveIsotope[i][1];
+                    break;
+                }
+    
+                
+    
+            }
+    
+            return pages;
+        }
+        
+        function setPagination() {
+    
+            var SettingsPagesOnItems = function(){
+    
+                var itemsLength = $container.children(itemSelector).length;
+                
+                var pages = Math.ceil(itemsLength / itemsPerPage);
+                var item = 1;
+                var page = 1;
+                var selector = itemSelector;
+                    selector += ( currentFilter != '*' ) ? currentFilter : '';
+                
+                $container.children(selector).each(function(){
+                    if( item > itemsPerPage ) {
+                        page++;
+                        item = 1;
+                    }
+                    $(this).attr(pageAtribute, page);
+                    item++;
+                });
+    
+                currentNumberPages = page;
+    
+            }();
+    
+            var CreatePagers = function() {
+    
+                var $isotopePager = ( $('.'+pagerClass).length == 0 ) ? $('<div class="'+pagerClass+'"></div>') : $('.'+pagerClass);
+    
+                $isotopePager.html('');
+                
+                for( var i = 0; i < currentNumberPages; i++ ) {
+                    var $pager = $('<a href="javascript:void(0);" class="pager" '+pageAtribute+'="'+(i+1)+'"></a>');
+                        $pager.html(i+1);
+                        
+                        $pager.click(function(){
+                            var page = $(this).eq(0).attr(pageAtribute);
+                            goToPage(page);
+                        });
+    
+                    $pager.appendTo($isotopePager);
+                }
+    
+                $container.after($isotopePager);
+    
+            }();
+    
+        }
+        
+        setPagination();
+        goToPage(1);
+    
+        //Adicionando Event de Click para as categorias
+        $('.filters a').click(function(e){
+            e.preventDefault();
+            var filter = $(this).attr(filterAtribute);
+            currentFilter = filter;
+    
+            setPagination();
+            goToPage(1);
+    
+    
+        });
+    
+        //Evento Responsivo
+        $(window).resize(function(){
+            itemsPerPage = defineItemsPerPage();
+            setPagination();
+            goToPage(1);
+        });
+    
+        $('.content').addClass('ready');
+    
+    });
 
 });
